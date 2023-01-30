fa56fd6b0a41968979390955c5f571405acc1bf3
Arlo Valiela <arlovaliela@Arlos-MacBook-Pro.local>
2023-01-30 16:37:53 -0500

    new button

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+ // Print
+
 console.log("Hello World");
+
+// Varialbes 
+
+// Constants const = , let = 
+
+let count = 0;
+
+function buttonClicked() {
+	count ++;
+	document.getElementById("button-div").innerHTML = count;
+
+
+}
+
